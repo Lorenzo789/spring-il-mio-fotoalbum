3b055bd9956cd2491858ac4aa9268feade1cc868
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-12-21 10:36:13 +0100

    feat add vue with axios

---
+++ src/main/java/org/generation/italy/demo/controller/api/FotoApiController.java
@@ -0,0 +1,28 @@
+package org.generation.italy.demo.controller.api;
+
+import java.util.List;
+
+import org.generation.italy.demo.pojo.Foto;
+import org.generation.italy.demo.service.FotoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/1/foto")
+@CrossOrigin("*")
+public class FotoApiController {
+
+	@Autowired
+	private FotoService fotoService;
+	
+	@GetMapping("/all")
+	public List<Foto> getAllFoto(){
+		
+		List<Foto> fotos = fotoService.findAll();
+		
+		return fotos;
+	}
+}
